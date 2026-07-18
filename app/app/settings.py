46a3e0454d3041a007e2325527b68a5affb94ccc
Maksym <maksim260306@gmail.com>
2026-07-19 00:01:52 +0300

    Add user uth, fix test and implement token creation

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'rest_framework.authtoken',
+    'drf_spectacular',
     'user',
 ]
 
@@ -130,3 +132,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
